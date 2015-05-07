331e72840aa2c58aa0b5dd3d33c18cddb9564d48
Murat Yirci <myirci@gmail.com>
2015-05-07 19:10:22 +0200

    mod.

--- src/modeller/optimization/ComponentSolver.cpp
+++ src/modeller/optimization/ComponentSolver.cpp
@@ -1,229 +1,56 @@
 #include <iostream>
+#include <cmath>
 #include "ComponentSolver.hpp"
-#include "../ProjectionParameters.hpp"
-#include "../../geometry/Circle3D.hpp"
-#include "../../utility/Utility.hpp"
 
-ComponentSolver::ComponentSolver(const std::shared_ptr<ProjectionParameters>& pp) : m_lfpts(nullptr), m_pp(pp) {
 
-    base_circles = new Circle3D[2];
-    base_circles[0].radius = 1.0;
-    base_circles[1].radius = 1.0;
+ComponentSolver::ComponentSolver(double near_) : m_lframe_proj(new osg::Vec2dArray(4)), near(near_), squared_near(near_ * near_) {
+
+    options.linear_solver_type = ceres::DENSE_QR;
+    options.minimizer_progress_to_stdout = true;
 }
 
-ComponentSolver::~ComponentSolver() {
-    delete[] base_circles;
+double ComponentSolver::func(int i, int j) {
+    return squared_near + m_lframe_proj->at(i).x() * m_lframe_proj->at(j).x() + m_lframe_proj->at(i).y() * m_lframe_proj->at(j).y();
 }
 
-Circle3D* ComponentSolver::GetBaseCircles() {
-    return base_circles;
+void ComponentSolver::calculate_coefficients() {
+
+    double f00 = func(0,0);
+    double f01 = func(0,1);
+    double f02 = func(0,2);
+    double f03 = func(0,3);
+    double f12 = func(1,2);
+    double f13 = func(1,3);
+    double f23 = func(2,3);
+
+    double a = f00*f12*f13 + f01*f01*f23 - f01*f02*f13 - f01*f03*f12;
+    double b = 2*f01*(f02*f03 - f00*f23);
+    double c = f00*(f00*f23 - f02*f03);
+
+    double dlt = b*b - 4*a*c;
+    std::cout << "discriminant: " << dlt << std::endl;
+    double d1 = (-b + std::sqrt(dlt)) / (2*a);
+    double d2 = (-b - std::sqrt(dlt)) / (2*a);
+    std::cout << "d1: " << d1 << std::endl;
+    std::cout << "d2: " << d2 << std::endl;
+    coeff[0] = d1; // or d2
+    coeff[2] = (coeff[0]*f01 - f00) / (coeff[0]*f13 - f03);
+    coeff[1] = (coeff[2]*f03 - f00) / (coeff[2]*f23 - f02);
+
+    C1v[0] = (coeff[0] * m_lframe_proj->at(1).x() + coeff[1] * m_lframe_proj->at(2).x()) / 2*near;
+    C1v[1] = (coeff[0] * m_lframe_proj->at(1).y() + coeff[1] * m_lframe_proj->at(2).y()) / 2*near;
+    C1v[2] = (coeff[0] + coeff[1]) / 2.0;
+
+    C2v[0] = C1v[0] + (coeff[2] * m_lframe_proj->at(3).x() - m_lframe_proj->at(0).x()) / near;
+    C2v[1] = C1v[1] + (coeff[2] * m_lframe_proj->at(3).y() - m_lframe_proj->at(0).y()) / near;
+    C2v[2] = C1v[2] + coeff[2] - 1;
 }
 
-void ComponentSolver::SolveDepthValues() const {
+void ComponentSolver::Solve(double& Z0, double* C1, double* C2) {
 
-    if(!m_lfpts.valid()) {
-        std::cout << "Coordinate frame points have not been updated" << std::endl;
-        return;
-    }
-
-    // For base_circle_1:
-    double k = std::sqrt(-F(1, 2) * F(1, 3) * F(2, 3));
-    double a1 = (1/F(2, 3)) * k; // z1 = z0 +- a1
-    double a2 = (1/F(1, 3)) * k; // z2 = z0 +- a2
-    double a3 = (1/F(1, 2)) * k; // z3 = z0 +- a3
-
-    osg::Vec3d cbt, ctp, P0, P1, P2, P3;
-
-    auto z01 = quadratic_root_finder_x(base_circles[0].center[0], a1, a2);
-    calculate_P0(P0, z01.first);
-    calculate_P1(P1, z01.first + a1);
-    calculate_P2(P2, z01.first + a2);
-    calculate_P3(P3, z01.first + a3);
-    calculate_bottom_center_point(cbt, z01.first+a1, z01.first+a2);
-    calculate_top_center_point(cbt, ctp, z01.first, z01.first+a3);
-
-    calculate_P0(P0, z01.second);
-    calculate_P1(P1, z01.second + a1);
-    calculate_P2(P2, z01.second + a2);
-    calculate_P3(P3, z01.second + a3);
-    calculate_bottom_center_point(cbt, z01.second+a1, z01.second+a2);
-    calculate_top_center_point(cbt, ctp, z01.second, z01.second+a3);
-
-    auto z04 = quadratic_root_finder_x(base_circles[0].center[0], -a1, -a2);
-    calculate_P0(P0, z04.first);
-    calculate_P1(P1, z04.first - a1);
-    calculate_P2(P2, z04.first - a2);
-    calculate_P3(P3, z04.first - a3);
-    calculate_bottom_center_point(cbt, z04.first-a1, z04.first-a2);
-    calculate_top_center_point(cbt, ctp, z04.first, z04.first-a3);
-
-    calculate_P0(P0, z04.second);
-    calculate_P1(P1, z04.second - a1);
-    calculate_P2(P2, z04.second - a2);
-    calculate_P3(P3, z04.second - a3);
-    calculate_bottom_center_point(cbt, z04.second-a1, z04.second-a2);
-    calculate_top_center_point(cbt, ctp, z04.second, z04.second-a3);
-
-    auto z05 = quadratic_root_finder_x(base_circles[1].center[0], a1, a2);
-    calculate_P0(P0, z05.first);
-    calculate_P1(P1, z05.first + a1);
-    calculate_P2(P2, z05.first + a2);
-    calculate_P3(P3, z05.first + a3);
-    calculate_bottom_center_point(cbt, z05.first+a1, z05.first+a2);
-    calculate_top_center_point(cbt, ctp, z05.first, z05.first+a3);
-
-    calculate_P0(P0, z05.second);
-    calculate_P1(P1, z05.second + a1);
-    calculate_P2(P2, z05.second + a2);
-    calculate_P3(P3, z05.second + a3);
-    calculate_bottom_center_point(cbt, z05.second+a1, z05.second+a2);
-    calculate_top_center_point(cbt, ctp, z05.second, z05.second+a3);
-
-    auto z08 = quadratic_root_finder_x(base_circles[1].center[0], -a1, -a2);
-    calculate_P0(P0, z08.first);
-    calculate_P1(P1, z08.first - a1);
-    calculate_P2(P2, z08.first - a2);
-    calculate_P3(P3, z08.first - a3);
-    calculate_bottom_center_point(cbt, z08.first-a1, z08.first-a2);
-    calculate_top_center_point(cbt, ctp, z08.first, z08.first-a3);
-
-    calculate_P0(P0, z08.second);
-    calculate_P1(P1, z08.second - a1);
-    calculate_P2(P2, z08.second - a2);
-    calculate_P3(P3, z08.second - a3);
-    calculate_bottom_center_point(cbt, z08.second-a1, z08.second-a2);
-    calculate_top_center_point(cbt, ctp, z08.second, z08.second-a3);
-
- /*
-    auto z01 = quadratic_root_finder_y(base_circles[0].center[1], a1, a2);
-    calculate_bottom_center_point(cbt, z01.first+a1, z01.first+a2);
-    calculate_bottom_center_point(cbt, z01.second+a1, z01.second+a2);
-    auto z04 = quadratic_root_finder_y(base_circles[0].center[1], -a1, -a2);
-    calculate_bottom_center_point(cbt, z04.first-a1, z04.first-a2);
-    calculate_bottom_center_point(cbt, z04.second-a1, z04.second-a2);
-    auto z05 = quadratic_root_finder_y(base_circles[1].center[1], a1, a2);
-    calculate_bottom_center_point(cbt, z05.first+a1, z05.first+a2);
-    calculate_bottom_center_point(cbt, z05.second+a1, z05.second+a2);
-    auto z08 = quadratic_root_finder_y(base_circles[1].center[1], -a1, -a2);
-    calculate_bottom_center_point(cbt, z08.first-a1, z08.first-a2);
-    calculate_bottom_center_point(cbt, z08.second-a1, z08.second-a2);
-
-*/
-    /*
-    auto z01 = quadratic_root_finder_z(base_circles[0].center[2], a1, a2);
-    calculate_bottom_center_point(cbt, z01.first+a1, z01.first+a2);
-    calculate_top_center_point(cbt, ctp, z01.first+a1, z01.first+a3);
-    calculate_bottom_center_point(cbt, z01.second+a1, z01.second+a2);
-    calculate_top_center_point(cbt, ctp, z01.second+a1, z01.second+a3);
-    auto z04 = quadratic_root_finder_z(base_circles[0].center[2], -a1, -a2);
-    calculate_bottom_center_point(cbt, z04.first-a1, z04.first-a2);
-    calculate_top_center_point(cbt, ctp, z04.first-a1, z04.first-a3);
-    calculate_bottom_center_point(cbt, z04.second-a1, z04.second-a2);
-    calculate_top_center_point(cbt, ctp, z04.second-a1, z04.second-a3);
-    auto z05 = quadratic_root_finder_z(base_circles[1].center[2], a1, a2);
-    calculate_bottom_center_point(cbt, z05.first+a1, z05.first+a2);
-    calculate_top_center_point(cbt, ctp, z05.first+a1, z05.first+a3);
-    calculate_bottom_center_point(cbt, z05.second+a1, z05.second+a2);
-    calculate_top_center_point(cbt, ctp, z05.second+a1, z05.second+a3);
-    auto z08 = quadratic_root_finder_z(base_circles[1].center[2], -a1, -a2);
-    calculate_bottom_center_point(cbt, z08.first-a1, z08.first-a2);
-    calculate_top_center_point(cbt, ctp, z08.first-a1, z01.first-a3);
-    calculate_bottom_center_point(cbt, z08.second-a1, z08.second-a2);
-    calculate_top_center_point(cbt, ctp, z08.second-a1, z08.second-a3);
-    */
-}
-
-double ComponentSolver::F(int i, int j) const {
-
-    return m_pp->far * m_pp->far *
-           ((m_lfpts->at(0).x()*m_lfpts->at(0).x() + m_lfpts->at(0).y()*m_lfpts->at(0).y()) - // E00
-            (m_lfpts->at(0).x()*m_lfpts->at(i).x() + m_lfpts->at(0).y()*m_lfpts->at(i).y()) - // E0i
-            (m_lfpts->at(0).x()*m_lfpts->at(j).x() + m_lfpts->at(0).y()*m_lfpts->at(j).y()) + // E0j
-            (m_lfpts->at(i).x()*m_lfpts->at(j).x() + m_lfpts->at(i).y()*m_lfpts->at(j).y())   // Eij
-           );
-}
-
-std::pair<double, double> ComponentSolver::quadratic_root_finder_x(double x, double a1, double a2) const {
-
-    double g = m_pp->far + m_pp->near;
-    double a = 2*x;
-    double b = a*(a1+a2+2*g) - m_pp->far*(m_lfpts->at(1).x() + m_lfpts->at(2).x());
-    double c = a*(a1*a2+g*(a1+a2+g)) - m_pp->far*(m_lfpts->at(1).x()*(a2+g) + m_lfpts->at(2).x()*(a1+g));
-    double sqrt_delta = std::sqrt(b*b - 4*a*c);
-    return std::make_pair((-b + sqrt_delta) / (2*a), (-b - sqrt_delta) / (2*a));
-}
-
-std::pair<double, double> ComponentSolver::quadratic_root_finder_y(double y, double a1, double a2) const {
-
-    double g = m_pp->far + m_pp->near;
-    double a = 2*y;
-    double b = a*(a1+a2+2*g) - m_pp->far*(m_lfpts->at(1).y() + m_lfpts->at(2).y());
-    double c = a*(a1*a2+g*(a1+a2+g)) - m_pp->far*(m_lfpts->at(1).y()*(a2+g) + m_lfpts->at(2).y()*(a1+g));
-    double sqrt_delta = std::sqrt(b*b - 4*a*c);
-    return std::make_pair((-b + sqrt_delta) / (2*a), (-b - sqrt_delta) / (2*a));
-}
-
-std::pair<double, double> ComponentSolver::quadratic_root_finder_z(double z, double a1, double a2) const {
-
-    double g = m_pp->far + m_pp->near;
-    double a = 2*z;
-    double b = a*(a1+a2+2*g) + 2*m_pp->far*m_pp->near;
-    double c = a*(a1*a2+g*(a1+a2+g)) + m_pp->far*m_pp->near*(a1+a2+2*g);
-    double sqrt_delta = std::sqrt(b*b - 4*a*c);
-    return std::make_pair((-b + sqrt_delta) / (2*a), (-b - sqrt_delta) / (2*a));
-}
-
-void ComponentSolver::calculate_bottom_center_point(osg::Vec3d& cbt, double z1, double z2) const {
-
-    double g = m_pp->far + m_pp->near;
-    cbt.set(0.5*m_pp->far*(m_lfpts->at(1).x()/(z1+g) + m_lfpts->at(2).x()/(z2 + g)),
-            0.5*m_pp->far*(m_lfpts->at(1).y()/(z1+g) + m_lfpts->at(2).y()/(z2 + g)),
-           -0.5*m_pp->far*m_pp->near*(1/(z1+g) + 1/(z2+g)));
-}
-
-void ComponentSolver::calculate_top_center_point(const osg::Vec3d& cbt, osg::Vec3d& ctp, double z0, double z3) const {
-
-    double g = m_pp->far + m_pp->near;
-    ctp = cbt + osg::Vec3d(((m_pp->far*m_lfpts->at(3).x())/(z3+g)) - ((m_pp->far*m_lfpts->at(0).x())/(z0+g)),
-                           ((m_pp->far*m_lfpts->at(3).y())/(z3+g)) - ((m_pp->far*m_lfpts->at(0).y())/(z0+g)),
-                           ((m_pp->far*m_pp->near)/(z0+g)) - ((m_pp->far*m_pp->near)/(z3+g)));
-}
-
-void ComponentSolver::calculate_P0(osg::Vec3d& P0, double z0) const {
-
-    P0[0] = (m_pp->far  * m_lfpts->at(0).x()) / (z0 + m_pp->far + m_pp->near);
-    P0[1] = (m_pp->far  * m_lfpts->at(0).y()) / (z0 + m_pp->far + m_pp->near);
-    P0[2] = (-m_pp->far * m_pp->near)        / (z0 + m_pp->far + m_pp->near);
-}
-
-void ComponentSolver::calculate_P1(osg::Vec3d& P1, double z1) const {
-
-    P1[0] = (m_pp->far  * m_lfpts->at(1).x()) / (z1 + m_pp->far + m_pp->near);
-    P1[1] = (m_pp->far  * m_lfpts->at(1).y()) / (z1 + m_pp->far + m_pp->near);
-    P1[2] = (-m_pp->far * m_pp->near)        / (z1 + m_pp->far + m_pp->near);
-}
-
-void ComponentSolver::calculate_P2(osg::Vec3d& P2, double z2) const {
-
-    P2[0] = (m_pp->far*m_lfpts->at(2).x())/(z2+m_pp->far+m_pp->near);
-    P2[1] = (m_pp->far*m_lfpts->at(2).y())/(z2+m_pp->far+m_pp->near);
-    P2[2] = (-m_pp->far*m_pp->near)/(z2+m_pp->far+m_pp->near);
-}
-
-void ComponentSolver::calculate_P3(osg::Vec3d& P3, double z3) const {
-
-    P3[0] = (m_pp->far*m_lfpts->at(3).x())/(z3+m_pp->far+m_pp->near);
-    P3[1] = (m_pp->far*m_lfpts->at(3).y())/(z3+m_pp->far+m_pp->near);
-    P3[2] = (-m_pp->far*m_pp->near)/(z3+m_pp->far+m_pp->near);
-}
-
-double ComponentSolver::test_orthogonality(const osg::Vec3d& P0, const osg::Vec3d& P1, const osg::Vec3d& P2, const osg::Vec3d& P3) const {
-    return std::abs((P1 - P0)*(P2 - P0)) + std::abs((P1 - P0)*(P3 - P0)) + std::abs((P2 - P0)*(P3 - P0));
-}
-
-double ComponentSolver::test_orthogonality(const osg::Vec3d& cbt, const osg::Vec3d& ctp, int base) const {
-    osg::Vec3d normal(base_circles[base].normal[0], base_circles[base].normal[1], base_circles[base].normal[2]);
-    osg::Vec3d spine = ctp - cbt;
-    double angle = std::acos((spine * normal) /(spine.length() * normal.length()));
-    return rad2deg(angle);
+    ceres::Problem problem;
+    ceres::CostFunction* cost_function = new ceres::AutoDiffCostFunction<CostFunctor, 1, 1, 3, 3>(new CostFunctor(C1v, C2v));
+    problem.AddResidualBlock(cost_function, NULL, &Z0, C1, C2);
+    ceres::Solve(options, &problem, &summary);
+    std::cout << summary.BriefReport() << "\n";
 }
